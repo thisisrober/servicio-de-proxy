74b45023423fe611970de96ad4a331bb0860bf32
ireeneariaas <ireeneariaas@gmail.com>
2025-02-20 09:40:40 +0100

    guardar y restablecer

--- scripts/script-irene.sh
+++ scripts/script-irene.sh
@@ -19,3 +19,7 @@ echo "Configurando Squid..."
 echo "acl red_local src 172.16.111.0/24" | sudo tee -a $SQUID_CONF
 echo "http_access allow red_local" | sudo tee -a $SQUID_CONF
 echo "http_port 3128" | sudo tee -a $SQUID_CONF
+
+#reiniciamos el archivo y comprobamos que el squid funcione correctamente.
+sudo systemctl restart squid.service
+squid -k parse
