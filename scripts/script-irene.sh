ac15f95487ea8335b2766d1675bb3f31bf97413a
ireeneariaas <ireeneariaas@gmail.com>
2025-02-20 09:46:52 +0100

    reiniciamos servicio

--- scripts/script-irene.sh
+++ scripts/script-irene.sh
@@ -15,7 +15,7 @@ fi
 SQUID_CONF="/etc/squid/squid.conf"
 
 #configuramos el squid para que funcione como proxy para nuestra red interna
-echo "Configurando Squid..."
+echo "Configurando Squid"
 echo "acl red_local src 172.16.111.0/24" | sudo tee -a $SQUID_CONF
 echo "http_access allow red_local" | sudo tee -a $SQUID_CONF
 echo "http_port 3128" | sudo tee -a $SQUID_CONF
@@ -31,3 +31,7 @@ echo -e "youtube.com" | sudo tee $BLOCKLIST
 
 echo "acl sitios_prohibidos dstdomain "/etc/squid/blocklist.txt"" | sudo tee -a $SQUID_CONF
 echo "http_access deny sitios_prohibidos" | sudo tee -a $SQUID_CONF
+
+#reiniciamos el servicio nuevamente
+sudo systemctl restart squid
+
