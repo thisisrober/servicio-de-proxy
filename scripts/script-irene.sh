4a703e04d1c7cc0a29fca5ea0aef3442acbf0b31
ireeneariaas <ireeneariaas@gmail.com>
2025-02-20 09:21:40 +0100

    instalacion squid

--- scripts/script-irene.sh
+++ scripts/script-irene.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+#actualizamos el sistema
 sudo apt update && sudo apt upgrade
+
+#verificamos que este todo actualizado e instalamos squid
+if [ $? -eq 0 ]; then
+    sudo apt install -y squid squidguard
+else
+    echo "Actualizacion con errores"
+    exit 1 #indica un error
+fi
+
