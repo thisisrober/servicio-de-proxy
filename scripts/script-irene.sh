f46c44f339e3a2da20dbcbf0c6b93c7362a5ce20
ireeneariaas <ireeneariaas@gmail.com>
2025-02-20 09:36:20 +0100

    squid funcione como proxy

--- scripts/script-irene.sh
+++ scripts/script-irene.sh
@@ -14,3 +14,8 @@ fi
 #configuramos el archivo de configuracion del squid
 SQUID_CONF="/etc/squid/squid.conf"
 
+#configuramos el squid para que funcione como proxy para nuestra red interna
+echo "Configurando Squid..."
+echo "acl red_local src 172.16.111.0/24" | sudo tee -a $SQUID_CONF
+echo "http_access allow red_local" | sudo tee -a $SQUID_CONF
+echo "http_port 3128" | sudo tee -a $SQUID_CONF
